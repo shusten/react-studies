1b055223473451105e6a922db31ba7afa53b299e
Luis Villas-Boas <shusten@hotmail.com>
2024-05-17 00:51:38 -0300

    Todo test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-export function App() {
+const data = [
+  { 
+    id: 1,
+    title: 'Meu primeiro todo',
+    isCompleted: 'false',
+  },
+  { 
+    id: 2,
+    title: 'Meu primeiro todo',
+    isCompleted: 'false',
+  },
+  { 
+    id: 3,
+    title: 'Meu primeiro todo',
+    isCompleted: 'false',
+  },
+]
 
+
+export function App() {
   return (
    <main>
       <h1>Todo App</h1>
       <input type="text" />
       <button>Add</button>
+      {data.map(item => (
+        <p key={item.id}>{item.id}. {item.title}</p>
+      ))}
    </main>
   )
 }
