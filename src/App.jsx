0d5ff6fdf3607df4e9c382e0a54deec188b10481
Luis Villas-Boas <shusten@hotmail.com>
2024-05-17 02:07:05 -0300

    Add list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-const data = [
-  { 
-    id: 1,
-    title: 'Meu primeiro todo',
-    isCompleted: 'false',
+import { Task } from "./components/Task"
+
+const tasks = [
+ {  id: 1,
+    title: "Estudar ",
+    inCompleted: 'false'
   },
-  { 
-    id: 2,
-    title: 'Meu primeiro todo',
-    isCompleted: 'false',
-  },
-  { 
-    id: 3,
-    title: 'Meu primeiro todo',
-    isCompleted: 'false',
-  },
+ {  id: 2,
+    title: "Estudar ",
+    inCompleted: 'false'
+  }
 ]
 
 
@@ -23,9 +18,11 @@ export function App() {
       <h1>Todo App</h1>
       <input type="text" />
       <button>Add</button>
-      {data.map(item => (
-        <p key={item.id}>{item.id}. {item.title}</p>
+      {tasks.length > 0 && tasks.map(item => (
+        <Task key={item.id} task={item} />
       ))}
+
+      {!tasks.length && <p>Nenhuma tarefa ainda... 😕 </p>}
    </main>
   )
 }
---
+++ src/components/Task/index.jsx
@@ -0,0 +1,5 @@
+export function Task(props) {
+    return (
+        <p>{props.task.id}. {props.task.title}</p>
+    )
+}
